60fec1a61fed112fd5c54dbcf6607d7d9aebf16e
Robby Dore <Rdore88@gmail.com>
2017-03-30 08:56:17 -0400

    fixed part of the new user function.

--- Flight_Board/config/routes.rb
+++ Flight_Board/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :flight_routes do
     resources :flights
   end
-  get "users/new", controller: "users", action: "new"
+  get "/users/new", controller: "users", action: "new"
+
+  post "/users", controller: "users", action: "create"
 
   root controller: "flight_routes", action: "index"
 
